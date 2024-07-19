6d80fb093d8763381bf05021b2b314d177d00fd4
karayilanberat <karayilanberat@gmail.com>
2024-07-19 21:28:50 +0300

    End Of The Project

---
+++ Flash Chat iOS13/Messages.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by berat on 19.07.2024.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Messages {
+    let sender: String
+    let body: String
+}
